9826aacb07baba72a8263016d3d9aac6144c8621
Caden Fehr <cadenfehr@yahoo.ca>
2024-05-15 22:24:29 -0600

    Initial Commit

---
+++ WeatherTutorial/WeatherTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTutorialApp.swift
+//  WeatherTutorial
+//
+//  Created by Caden Fehr on 2024-05-15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

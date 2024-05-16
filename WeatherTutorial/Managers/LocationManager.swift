ea473e6a63b15c8b1e07674290ee364dc9970d60
PeachedTea <cadenfehr@yahoo.ca>
2024-05-15 22:49:01 -0600

    Location Manager - Implement requesting user's location using apples CoreLocation Framework - This code handles getting the users location to be used to grab the appropriate weather data - Error handling uses a print statement currently

--- WeatherTutorial/Managers/LocationManager.swift
+++ WeatherTutorial/Managers/LocationManager.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import CoreLocation // Apples location framework
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    // constants
+    let manager = CLLocationManager()
+    
+    // variables
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    // Function that requests the users location
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    // Function to check if the location was updated
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // set location var to array of location
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    // function to check if the location request threw an error
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isLoading = false
+    }
+}

e4f2a3c1cac23cbb45088cb5dc1afc40e2bc9269
Caden Fehr <cadenfehr@yahoo.ca>
2024-05-19 04:51:02 -0600

    Added LoadingView and coordinate display
    ContentView
    - Added LoadingView when loading
    - Added if statement for when location isn't shared
    
    LoadingView
    - Created a loading view
    
    Other
    - updated git Ignore

--- WeatherTutorial/Views/ContentView.swift
+++ WeatherTutorial/Views/ContentView.swift
@@ -11,8 +11,20 @@ struct ContentView: View {
     @StateObject var locationManager = LocationManager()
     var body: some View {
         VStack {
-            WelcomeView()
-                .environmentObject(locationManager)
+            
+            // if the location is given, display it
+            if let location = locationManager.location {
+                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+            }
+            else {
+                if locationManager.isLoading {
+                    LoadingView()
+                }
+                else {
+                }
+                WelcomeView()
+                    .environmentObject(locationManager)
+            }
         }
         .background(.black)
         .preferredColorScheme(.dark)
---
+++ WeatherTutorial/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  WeatherTutorial
+//
+//  Created by Caden Fehr on 2024-05-19.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+        .progressViewStyle(CircularProgressViewStyle(tint: .orange))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}

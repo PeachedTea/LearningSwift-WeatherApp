746da3b931229be2c8935de01f28213c673e04b6
Caden Fehr <cadenfehr@yahoo.ca>
2024-05-20 16:45:03 -0600

    Completed WeatherManager
    WeatherManager
    - Completed WeatherManager class which manages the requests and returns from the API
    
    - Created a structure to decode the JSON information sent from the API
    
    - Added comments to code

--- WeatherTutorial/Managers/WeatherManager.swift
+++ WeatherTutorial/Managers/WeatherManager.swift
@@ -8,17 +8,74 @@
 import Foundation
 import CoreLocation
 
+/*
+ WeatherManager
+ @return: ResponseBody
+ 
+ This class manages the requests and returns from the OpenWeather API and decodes it
+ into and returns a ResponseBody structure which stores the data
+ */
 class WeatherManager {
-    func getCurrentWeather(latitude: CLLocation, longitude: CLLocation) async throws {
+    func getCurrentWeather(latitude: CLLocation, longitude: CLLocation) async throws -> ResponseBody {
+        
+        // url for the API call
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=2d48206ea5bde563a613a90b474b8dc7&unitsmetric") else {fatalError("Missing URL")}
         
+        // request the data from the url
         let urlRequest = URLRequest(url: url)
         
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
         
         guard ( response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data")}
+        
+        // decode the JSON data into a ResponseBody structure
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        // return the ResponseBody structure with data from the API call
+        return decodedData
+    }
+    
+}
 
+// structure used to store the JSON information sent from an API call to OpenWeather
+struct ResponseBody: Decodable {
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+    
+    struct CoordinatesResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    
+    }
+    
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
         
     }
     
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
+}
+
+extension ResponseBody.MainResponse {
+    var feelsLike: Double {return feels_like}
+    var tempMin: Double {return temp_min}
+    var tempMax: Double {return temp_max}
 }
